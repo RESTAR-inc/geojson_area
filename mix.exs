36ccb7674c43bc3626bfccb1466eb552b1c78aa0
Jean Klingler <jean.klingler@restar-inc.com>
2021-08-16 16:38:04 +0900

    Setup CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GeoJSON.Area.MixProject do
     [
       app: :geojson_area,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       preferred_cli_env: [
@@ -14,7 +14,8 @@ defmodule GeoJSON.Area.MixProject do
         dialyzer: :test
       ],
       package: package(),
-      description: "Compute the area of GeoJSON geometries"
+      description: "Compute the area of GeoJSON geometries",
+      dialyzer: dialyzer()
     ]
   end
 
@@ -37,6 +38,13 @@ defmodule GeoJSON.Area.MixProject do
     [
       {:ex_doc, "~> 0.25.1", only: :docs, runtime: false},
       {:dialyxir, "~> 1.0", only: :test, runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
 end
